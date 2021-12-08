8a0f1aa59eca0e009b253b03eafc83a99d985d7c
Vladimir <timotijevicvlada@gmail.com>
2021-12-08 23:29:58 +0100

    Added new project. CV updated.

--- src/data.js
+++ src/data.js
@@ -25,66 +25,72 @@ export const projects = [
     },
     {
         id: 5,
+        name: "Quiz App",
+        img: "quiz_app.JPG",
+        link: "https://quiz-app-vlada.netlify.app/"
+    },
+    {
+        id: 6,
         name: "Crypto App",
         img: "cryptoapp.JPG",
         link: "https://crypto-app-vladimir.netlify.app/"
     },
     {
-        id: 6,
+        id: 7,
         name: "Population ranks",
         img: "populationranks.JPG",
         link: "https://population-ranking.netlify.app/"
     },
     {
-        id: 7,
+        id: 8,
         name: "Weather forecast",
         img: "weather_forecast.JPG",
         link: "https://weather-forecast-vlada.netlify.app/"
     },
     {
-        id: 8,
+        id: 9,
         name: "To Do App",
         img: "todoapp.JPG",
         link: "https://timotijevic-vlada-to-do-app.netlify.app/"
     },
     {
-        id: 9,
+        id: 10,
         name: "Notes App",
         img: "notes_app.JPG",
         link: "https://timotijevic-vlada-noteapp.netlify.app/"
     },
     {
-        id: 10,
+        id: 11,
         name: "Shopping cart",
         img: "shopping_cart.JPG",
         link: "https://react-shopping-cart-by-vladimir.netlify.app/"
     },
     {
-        id: 11,
+        id: 12,
         name: "Weather App",
         img: "weatherapp.JPG",
         link: "https://timotijevic-vlada-weather-api.netlify.app/"
     },
     {
-        id: 12,
+        id: 13,
         name: "MovieDB",
         img: "moviedb.JPG",
         link: "https://timotijevic-vlada-moviedb.netlify.app/"
     },
     {
-        id: 13,
+        id: 14,
         name: "Breaking Bad",
         img: "breaking_bad.JPG",
         link: "https://timotijevic-vlada-breaking-bad-api.netlify.app/"
     },
     {
-        id: 14,
+        id: 15,
         name: "Phone shop",
         img: "phone_webshop.JPG",
         link: "https://timotijevic-vlada-phone-shop.netlify.app/"
     },
     {
-        id: 15,
+        id: 16,
         name: "Travel agency",
         img: "travel_agency.JPG",
         link: "https://timotijevic-vlada-travel-agency.netlify.app/"
